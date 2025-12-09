ba63f52eb1a2de26396bbe1b1905c4dcb065c2e0
GoldFrite <goldfrite@gmail.com>
2025-12-09 12:57:12 +0100

    Bump version to 2.0.0-beta.9
    Update version references in README, index.ts, package.json, and package-lock.json to 2.0.0-beta.9 for the new release.

--- index.ts
+++ index.ts
@@ -118,7 +118,7 @@ export { Launcher }
  *
  * ---
  *
- * @version 2.0.0-7
+ * @version 2.0.0-beta.9
  * @license MIT — See the `LICENSE` file for more information
  * @copyright Copyright (c) 2025, GoldFrite
  */
